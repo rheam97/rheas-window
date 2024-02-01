30094344502c9f7be10d60cd9240ea155d53a6c6
rheam97 <rheam97@gmail.com>
2024-01-31 21:03:30 -0500

    tiles  fix

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ function Tile (props:any){
 
   return (
     <>
-    <div onClick={()=>setShow(true)} className={`border-solid border-4 relative border-black bg-gradient-to-b ${props.color} to-transparent rounded-2xl blur-sm lg:max-2xl:min-h-[16rem] 2xl:min-h-[27rem] min-h-[9rem] ${props.project ? `${props.project} bg-cover hover:filter-none active:filter-none` : `hover:filter-none active:filter-none`} transition-filter ease-in-out duration-700`}>
+    <div onClick={()=>setShow(true)} className={`border-solid border-4 relative border-black bg-gradient-to-b ${props.color} to-transparent rounded-2xl blur-sm lg:min-h-[16rem] 2xl:min-h-[27rem] min-h-[9rem] ${props.project ? `${props.project} bg-cover hover:filter-none active:filter-none` : `hover:filter-none active:filter-none`} transition-filter ease-in-out duration-700`}>
     {props.project ? <ProjectModal show={show} title={props.title} github={props.github} deployed={props.deployed}/> : null }
     {/* {props.project ? <div className={`absolute bg-opacity-0 top-0 left 0`}></div> : null} */}
     </div>
