efc3d6f7729b87537f1ea52a9a9118ffac272fb1
rheam97 <rheam97@gmail.com>
2025-01-04 16:32:51 -0500

    fixed modal with usecontaxt and ts changes

--- app/tile.tsx
+++ app/tile.tsx
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import ProjectModal from "./ProjectModal";
 
-export const ModalContext = React.createContext(false)
+
+
+type ModalContextType = {
+  showModal: boolean,
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ModalContextState = {
+  showModal: false,
+  setShowModal: () => !ModalContextState.showModal
+}
+
+export const ModalContext = React.createContext<ModalContextType>(ModalContextState)
 
 function Tile(props: any) {
-  const [show, setShow] = useState(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   return (
     <>
-      <div
-        onClick={()=>setShow(true)}
-        className={`border-solid border-4 relative border-black bg-gradient-to-b ${
-          props.color
-        } to-transparent rounded-2xl blur-sm lg:min-h-[16rem] 2xl:min-h-[27rem] min-h-[9rem] ${
-          props.project
-            ? `${props.project} bg-cover hover:filter-none active:filter-none`
-            : `hover:filter-none active:filter-none`
-        } transition-filter ease-in-out duration-700`}
-      >
+      <ModalContext.Provider value={{ showModal, setShowModal }}>
+        <div
+          onClick={() => setShowModal(!showModal)}
+          className={`border-solid border-4 relative border-black bg-gradient-to-b ${props.color
+            } to-transparent rounded-2xl blur-sm lg:min-h-[16rem] 2xl:min-h-[27rem] min-h-[9rem] ${props.project
+              ? `${props.project} bg-cover hover:filter-none active:filter-none`
+              : `hover:filter-none active:filter-none`
+            } transition-filter ease-in-out duration-700`}
+        >
+
+        </div>
         {props.project ? (
-          <ProjectModal 
+          <ProjectModal
             title={props.title}
             github={props.github}
-            deployed={props.deployed}/>
+            deployed={props.deployed} />
         ) : null}
-      </div>
+      </ModalContext.Provider>
     </>
   );
 }
 
-  export default Tile;
+export default Tile;
